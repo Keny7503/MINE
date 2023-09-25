528a12b294703f28052695061fed62e96b2ee528
keny <keny7503@gmail.com>
2023-09-25 20:13:16 +0700

    Create Board.cs

---
+++ MINE/Board.cs
@@ -0,0 +1,24 @@
+using Avalonia.Controls;
+
+namespace MINE;
+
+public class Board
+{
+    private int column;
+    private int row;
+    public Button[][] btnBoard;
+
+
+
+    public void SetTable(int[][] table)
+    {
+           
+    }
+
+
+    public Board(int column, int row)
+    {
+        
+    }
+    
+}
--- MINE/MainWindow.axaml.cs
+++ MINE/MainWindow.axaml.cs
@@ -9,5 +9,10 @@ public partial class MainWindow : Window
         InitializeComponent();
         
         //Test comment
+        int[][] placeHolderTable = { 
+            new int[]{1,9,2},
+            new int[]{1,2,9}, 
+            new int[]{0,1,1}};
+        
     }
 }
