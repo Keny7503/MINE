bcfd930ddc1d11c8e30c271da82e4414d7fe577f
Imuseless123 <stickboy7777@gmail.com>
2023-09-26 14:51:33 +0700

    add data file
    add Celldata.cs
    add Boarddata.cs

---
+++ MINE/Data/CellData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace MINE.Data
+{
+	public class CellData
+	{
+		protected int RowIndex;
+		protected int ColIndex;
+		protected bool isMine;			//determine the cell has mine or not
+		protected int surroundingMine;  //determine the number of mine surrounding the cell
+        public CellData()
+		{
+			isMine = false;
+			surroundingMine = 0;
+        }
+		public bool IsMine
+		{
+			get { return isMine; }
+			set { isMine = value; }
+		}
+		public int SurroundingMine
+		{
+			get { return surroundingMine; }
+			set { surroundingMine = value; }
+		}
+		public void getIndex(int Row, int Col)
+		{
+			RowIndex = Row;
+			ColIndex = Col;
+        }
+	}
+}
+
