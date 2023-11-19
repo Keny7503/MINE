7d4961d001b1fb551bdc8b77d111491da57ab621
Imuseless123 <stickboy7777@gmail.com>
2023-11-19 10:39:31 +0700

    clickable menu
    modify menu.cs:
    
    + clickable difficulty option
    
    modify BoardData:
    
    + restructure code
    
    + add new difficulty
    
    modify MainWindow.axaml:
    
    + delete excess code

--- MINE/MainWindow.axaml.cs
+++ MINE/MainWindow.axaml.cs
@@ -36,33 +36,11 @@ public partial class MainWindow : Window
         //    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
         //    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
         //};
-        
-        
-        BoardData Board = new BoardData(row, column);
-
-        //is this to save the number of mines
-        int numberOfMine = -1;
-
-        //set the difficulty, place the mine, numbering the cell
-        CellData[,] BoardData = Board.CreateBoard("medium",ref numberOfMine);
-
-        //is this to save the empty cell
-        int row_th = 0;
-        int col_th = 0;
-
-        //create the UIboard
-        int[,] UIBoard = Board.UIBoard(BoardData, ref row_th, ref col_th);
+       
 
 
-        UI.Menu menuGame = new UI.Menu(UIBoard,row_th,col_th);
+        UI.Menu menuGame = new UI.Menu();
         RootPanel.Children.Add(menuGame);
-        
-
-        //var board = new Board(row,column);
-        //RootPanel.Children.Add(board);
-        //board.SetTable(UIBoard);
-
-
         
         // Uncomment the below if you want to see the hold board
         // board.RevealAll();
